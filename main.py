83556adc1f90968fbd36b434526d615b7207e67b
volchenkov <d.volchenkov@dataduck.com>
2023-09-27 20:58:46 +0300

    feat: dismissable round and game result modals

--- main.py
+++ main.py
@@ -393,9 +393,9 @@ def handle_make_project_deposit(data):
                 "UPDATE rooms SET game=NULL, number_of_games=number_of_games+1 WHERE uid=:room_id",
                 {"room_id": data["room_id"]},
             )
-            rating, winners = define_rating_and_winners(game)
+            rating = define_rating(game)
             emit(
-                "game_ended", {"rating": rating, "winners": winners}, to=data["room_id"]
+                "game_ended", {"rating": rating}, to=data["room_id"]
             )
         else:
             store_room_game(room_id, game)
@@ -439,24 +439,15 @@ def handle_select_team_for_round(data):
     emit("team_for_round_selected", {"game": game}, to=data["room_id"])
 
 
-def define_rating_and_winners(game: dict) -> tuple[list, list]:
+def define_rating(game: dict):
     """Returns rating of all players in the game and winner(s).
     If multiple players have the same max number of points, they all are winners.
     """
 
     all_players_points = game["all_players_points"]
     rating = sorted(all_players_points.items(), key=lambda item: int(item[1]))[::-1]
-    winners = [
-        name
-        for name, points in rating
-        if points == max([points for _, points in rating])
-    ]
-    # winners = [
-    #     k
-    #     for k, v in all_players_points.items()
-    #     if v == max(all_players_points.values())
-    # ]
-    return rating, winners
+
+    return rating
 
 
 if __name__ == "__main__":
